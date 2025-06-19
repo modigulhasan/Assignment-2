1aac8a233a548eb713ec7f34559172dda8fbe930
modigulhasan <gulhasan_modi@msn.com>
2025-06-19 15:13:36 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,8 @@
+#Task 1
+a=input('Enter any Value: ')
+a=int(a)
+
+if((a%2)==1):
+    print(a,' is a Odd Number')
+else:
+    print(a,' is a Even Number')
---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Sum of Integer Number 1 to 50
+
+b=0
+c=50
+a=range(b,51)
+for i in a:
+    b+=i
+
+print('The sum of numbers from  1 to 50 is: ',b)
